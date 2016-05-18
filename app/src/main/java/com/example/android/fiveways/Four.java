1b82d383802da46105d7bbca23a49bbefe138b9c
BocaMihnea <mihneaboca@yahoo.com>
2016-05-18 16:00:54 +0300

    First commit

---
+++ app/src/main/java/com/example/android/fiveways/Four.java
@@ -0,0 +1,15 @@
+package com.example.android.fiveways;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Tex on 5/17/2016.
+ */
+public class Four extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.four);
+    }
+}
---
+++ app/src/main/java/com/example/android/fiveways/Three.java
@@ -0,0 +1,15 @@
+package com.example.android.fiveways;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Tex on 5/17/2016.
+ */
+public class Three extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.three);
+    }
+}
