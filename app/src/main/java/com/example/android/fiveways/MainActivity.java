fbc85327f79da681d74750a3c470de21df070d70
BocaMihnea <mihneaboca@yahoo.com>
2016-05-22 00:51:58 +0300

    Update 4

--- app/src/main/java/com/example/android/fiveways/MainActivity.java
+++ app/src/main/java/com/example/android/fiveways/MainActivity.java
@@ -34,12 +34,17 @@ public class MainActivity extends AppCompatActivity {
     else if(button_text.equals("Cheat"))
     {Intent intent = new Intent(this,Five.class);
         startActivity(intent);}
+    else if(button_text.equals("Result"))
+    {Intent intent = new Intent(this,Result.class);
+        startActivity(intent);}
     else if(button_text.equals("Exit"))
     { this.finish();
         Intent intent = new Intent(Intent.ACTION_MAIN);
         intent.addCategory(Intent.CATEGORY_HOME);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
+        int pid = android.os.Process.myPid();
+        android.os.Process.killProcess(pid);
     }
 
     }
